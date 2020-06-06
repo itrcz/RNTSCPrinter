f290a3fddd26f7be22ffece48e799135839c3420
Илья Трикоз <i.trikoz@abr-region.ru>
2020-06-06 16:59:37 +0300

    fix example

--- example/App.tsx
+++ example/App.tsx
@@ -13,7 +13,7 @@ import { SafeAreaView, StatusBar, Alert, View, Button } from 'react-native';
 import TSCPrinter from 'rn-tsc-printer'
 
 const printer = new TSCPrinter({
-  ip: '192.168.31.39',
+  ip: '192.168.1.39',
   port: 9100,
   width: 60,
   height: 30,
@@ -33,23 +33,9 @@ const App = () => {
               if (busy) return
               try {
                 setBusy(true)
-                await printer.openport()
-                const e = await printer.printerstatus(1000)
-                await printer.clearbuffer()
+                await printer.open()
+                await printer.clear()
                 await printer.setup()
-                await printer.printerfont({
-                  x: 20, y: 20, font: '4', rotation: 0, text: "Заказ #4", zoomX: 1, zoomY: 1
-                })
-                await printer.printerfont({
-                  x: 20, y: 60, font: '2', rotation: 0, text: "Помидоры азербайджанские", zoomX: 1, zoomY: 1
-                })
-                await printer.printerfont({
-                  x: 20, y: 210, font: '3', rotation: 0, text: "ВЕС: 2,44 Кг", zoomX: 1, zoomY: 1
-                })
-                await printer.printerfont({
-                  x: 300, y: 210, font: '1', rotation: 0, text: "foods & goods", zoomX: 1, zoomY: 1
-                })
-                // await printer.sendcommand("TEXT 130,210,\"3\",0,1,1,\"foods & goods\"\r\n")
                 await printer.barcode({
                   x: 20,
                   y: 90,
@@ -58,12 +44,11 @@ const App = () => {
                   wide: 0,
                   printText: false,
                   rotation: 0,
-                  type: '93',
-                  code: '12345-12345-12345'
+                  type: '128',
+                  code: '1234567890'
                 })
-                await printer.printlabel(1,1)
-                await printer.closeport()
-                console.warn(e)
+                await printer.print(1,1)
+                await printer.close()
               } catch (error) {
                 console.error(error)
               } finally {
